7adf5f75ee26700fa79eb4d93625804a2074c168
Andrew Carr <aacarr5@gmail.com>
2017-09-13 13:08:54 -0500

    Create login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,20 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-  	binding.pry
-  	render 'new'
-  end
+	def create
+	  user = User.find_by(email: params[:session][:email])
+	  binding.pry
+	  if user && user.authenticate(params[:session][:password])
+	    session[:user_id] = user.id
+	    redirect_to root_url, notice: 'Logged in!'
+	  else
+	    flash.now.alert = 'Email or password is invalid'
+	    render :new
+	  end
+	end
 
-  def destroy
-  end
+	def destroy
+	  session[:user_id] = nil
+	  redirect_to root_url, notice: 'Logged out!'
+	end
 end
