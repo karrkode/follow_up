82a13747f42ab881577530ae03dc116e1e939b91
Andrew Carr <aacarr5@gmail.com>
2017-09-18 13:07:44 -0500

    Add user uniqueness

--- db/migrate/20170908210408_create_users.rb
+++ db/migrate/20170908210408_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     	t.string :first_name
     	t.string :last_name
 
-    	t.string :email
+    	t.string :email, uniqueness:true
 
     	t.string :affiliation 
     end
