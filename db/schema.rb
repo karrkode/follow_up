be2cbd11276a7685ce83fd7850845101244e3220
Andrew Carr <aacarr5@gmail.com>
2017-09-19 23:29:22 -0500

    Create membership model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170915222618) do
+ActiveRecord::Schema.define(version: 20170920011416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "followers", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "organization_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["organization_id"], name: "index_memberships_on_organization_id", using: :btree
+    t.index ["user_id"], name: "index_memberships_on_user_id", using: :btree
+  end
 
   create_table "organizations", force: :cascade do |t|
     t.string   "name"
@@ -26,7 +43,6 @@ ActiveRecord::Schema.define(version: 20170915222618) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "affiliation"
     t.string "password_digest"
   end
 
