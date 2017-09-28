3dd3e8cfbb7a45b85043b8fcc12e489425d2f51e
Andrew Carr <aacarr5@gmail.com>
2017-09-28 10:34:35 -0500

    Improve seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,21 @@ Organization.create!(name:"The People's Lobby", website:'http://www.thepeopleslo
 Organization.create!(name: 'Our Revolution', website:'https://ourrevolution.com/')
 
 me = User.create!(first_name:'Andrew',last_name:'Carr', email:'aacarr5@gmail.com',password:'password')
-5.times {Follower.create!(first_name:Faker::Name.first_name,last_name:Faker::Name.last_name,email:Faker::Internet.email,phone:Faker::PhoneNumber.cell_phone,organization_id:[1,2,3].sample,organizer_id:me.id)}
+Membership.create(user_id:me.id,organization_id:1)
+Membership.create(user_id:me.id,organization_id:2)
+Membership.create(user_id:me.id,organization_id:3)
+25.times {Follower.create!(first_name:Faker::Name.first_name,last_name:Faker::Name.last_name,email:Faker::Internet.email,phone:Faker::PhoneNumber.cell_phone,organization_id:[1,2,3].sample,organizer_id:me.id)}
 
 10.times do |count|
 	user = User.create!(first_name:Faker::Name.first_name,last_name:Faker::Name.last_name,email:Faker::Internet.email,password:'password')
-	20.times {Follower.create!(first_name:Faker::Name.first_name,last_name:Faker::Name.last_name,email:Faker::Internet.email,phone:Faker::PhoneNumber.cell_phone,organization_id:[1,2,3].sample,organizer_id:user.id)}
+	3.times do |sub_count|
+		Membership.create!(user_id:user.id,organization_id:sub_count+1)
+		5.times {Follower.create!(first_name:Faker::Name.first_name,last_name:Faker::Name.last_name,email:Faker::Internet.email,phone:Faker::PhoneNumber.cell_phone,organization_id:sub_count+1,organizer_id:user.id)}
+	end
+	# Membership.create!(user_id:count+1,organization_id:[1,2,3].sample) 
+	# 20.times {Follower.create!(first_name:Faker::Name.first_name,last_name:Faker::Name.last_name,email:Faker::Internet.email,phone:Faker::PhoneNumber.cell_phone,organization_id:[1,2,3].sample,organizer_id:user.id)}
 end
 
-11.times do |count|
-	Membership.create!(user_id:count+1,organization_id:[1,2,3].sample) 
-end  
+# 11.times do |count|
+# 	Membership.create!(user_id:count+1,organization_id:[1,2,3].sample) 
+# end  
