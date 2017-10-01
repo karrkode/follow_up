e76c146ea33e1c2f500dff8443ae8114d62893f5
Andrew Carr <aacarr5@gmail.com>
2017-10-01 00:32:03 -0500

    squash into note

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,4 @@
 class Note < ApplicationRecord
 	belongs_to :notable, polymorphic:true
+	belongs_to :author, class_name: 'User'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001014931) do
+ActiveRecord::Schema.define(version: 20171001023916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20171001014931) do
     t.text    "info"
     t.string  "notable_type"
     t.integer "notable_id"
+    t.integer "author_id"
   end
 
   create_table "organizations", force: :cascade do |t|
