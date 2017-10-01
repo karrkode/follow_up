46e4f5b614d56d5881b6c90158fb28399211e190
Andrew Carr <aacarr5@gmail.com>
2017-10-01 00:33:35 -0500

    Redirect to user show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 	  user = User.find_by(email: params[:session][:email])
 	  if user && user.authenticate(params[:session][:password])
 	    log_in(user)
-	    redirect_to root_url, notice: 'Logged in!'
+	    redirect_to user_path(user.id), notice: 'Logged in!'
 	  else
 	    flash[:danger] = 'Invalid email/password combination'
 	    render :new
