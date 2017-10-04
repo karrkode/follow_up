153cd6ca44f48dcd69975426f4855c68cec6ba3c
Andrew Carr <aacarr5@gmail.com>
2017-10-04 14:32:09 -0500

    Add address to follower

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001023916) do
+ActiveRecord::Schema.define(version: 20171004175731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20171001023916) do
     t.datetime "updated_at",      null: false
     t.string   "organizer_id"
     t.string   "organization_id"
+    t.string   "address"
   end
 
   create_table "memberships", force: :cascade do |t|
