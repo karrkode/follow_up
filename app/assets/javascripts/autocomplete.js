b888f5751f7510023afb9cbd47b72717485bb6c7
Andrew Carr <aacarr5@gmail.com>
2017-10-05 16:05:31 -0500

    initAutoComplete is not a function error

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -28,21 +28,22 @@ $(document).ready(function() {
 		console.log("fill in adddress change")
 	  // Get the place details from the autocomplete object.
 	  var place = autocomplete.getPlace();
+	  console.log(place)
 
-	  for (var component in componentForm) {
-	    document.getElementById(component).value = '';
-	    document.getElementById(component).disabled = false;
-	  }
+	  // for (var component in componentForm) {
+	  //   document.getElementById(component).value = '';
+	  //   document.getElementById(component).disabled = false;
+	  // }
 
-	  // Get each component of the address from the place details
-	  // and fill the corresponding field on the form.
-	  for (var i = 0; i < place.address_components.length; i++) {
-	    var addressType = place.address_components[i].types[0];
-	    if (componentForm[addressType]) {
-	      var val = place.address_components[i][componentForm[addressType]];
-	      document.getElementById(addressType).value = val;
-	    }
-	  }
+	  // // Get each component of the address from the place details
+	  // // and fill the corresponding field on the form.
+	  // for (var i = 0; i < place.address_components.length; i++) {
+	  //   var addressType = place.address_components[i].types[0];
+	  //   if (componentForm[addressType]) {
+	  //     var val = place.address_components[i][componentForm[addressType]];
+	  //     document.getElementById(addressType).value = val;
+	  //   }
+	  // }
 	}
 
 	// function geolocate() {
