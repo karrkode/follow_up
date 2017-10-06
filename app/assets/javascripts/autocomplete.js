7e90cf037caa5f81210b903af5d796214c5af024
Andrew Carr <aacarr5@gmail.com>
2017-10-06 15:47:02 -0500

    Drop down works

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,74 +1,73 @@
-$(document).ready(function() {
-	console.log('hello')
-	var placeSearch, autocomplete;
+console.log('hello')
+var placeSearch, autocomplete;
 
+
+
+var componentForm = {
+  street_number: 'short_name',
+  route: 'long_name',
+  locality: 'long_name',
+  administrative_area_level_1: 'short_name',
+  country: 'long_name',
+  postal_code: 'short_name'
+};
+
+
+function initAutocomplete() {
+	console.log('initiated')
+
+	
   autocomplete = new google.maps.places.Autocomplete(
       /** @type {!HTMLInputElement} */(document.getElementById('gmaps-input-address')),
       {types: ['geocode']});
 
   google.maps.event.addDomListener(window, 'load', initAutocomplete);
-
-  
-	var componentForm = {
-	  street_number: 'short_name',
-	  route: 'long_name',
-	  locality: 'long_name',
-	  administrative_area_level_1: 'short_name',
-	  country: 'long_name',
-	  postal_code: 'short_name'
-	};
-
-
-	function initAutocomplete() {
-		console.log('initiated')
-	  // Create the autocomplete object, restricting the search to geographical
-	  // location types.
+  // Create the autocomplete object, restricting the search to geographical
+  // location types.
 
 
 
-	  // When the user selects an address from the dropdown, populate the address
-	  // fields in the form.
-	  autocomplete.addListener('place_changed', fillInAddress);
-	}
+  // When the user selects an address from the dropdown, populate the address
+  // fields in the form.
+  autocomplete.addListener('place_changed', fillInAddress);
+}
 
 
-	function fillInAddress() {
-		console.log("fill in adddress change")
-	  // Get the place details from the autocomplete object.
-	  var place = autocomplete.getPlace();
-	  console.log(place)
+function fillInAddress() {
+	console.log("fill in adddress change")
+  // Get the place details from the autocomplete object.
+  var place = autocomplete.getPlace();
+  console.log(place)
 
-	  // for (var component in componentForm) {
-	  //   document.getElementById(component).value = '';
-	  //   document.getElementById(component).disabled = false;
-	  // }
+  // for (var component in componentForm) {
+  //   document.getElementById(component).value = '';
+  //   document.getElementById(component).disabled = false;
+  // }
 
-	  // // Get each component of the address from the place details
-	  // // and fill the corresponding field on the form.
-	  // for (var i = 0; i < place.address_components.length; i++) {
-	  //   var addressType = place.address_components[i].types[0];
-	  //   if (componentForm[addressType]) {
-	  //     var val = place.address_components[i][componentForm[addressType]];
-	  //     document.getElementById(addressType).value = val;
-	  //   }
-	  // }
-	}
+  // // Get each component of the address from the place details
+  // // and fill the corresponding field on the form.
+  // for (var i = 0; i < place.address_components.length; i++) {
+  //   var addressType = place.address_components[i].types[0];
+  //   if (componentForm[addressType]) {
+  //     var val = place.address_components[i][componentForm[addressType]];
+  //     document.getElementById(addressType).value = val;
+  //   }
+  // }
+}
 
-	// function geolocate() {
-	//   if (navigator.geolocation) {
-	//     navigator.geolocation.getCurrentPosition(function(position) {
-	//       var geolocation = {
-	//         lat: position.coords.latitude,
-	//         lng: position.coords.longitude
-	//       };
-	//       var circle = new google.maps.Circle({
-	//         center: geolocation,
-	//         radius: position.coords.accuracy
-	//       });
-	//       autocomplete.setBounds(circle.getBounds());
-	//     });
-	//   }
-	// }
-	console.log('goodbye')
-
-})
+// function geolocate() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//       var geolocation = {
+//         lat: position.coords.latitude,
+//         lng: position.coords.longitude
+//       };
+//       var circle = new google.maps.Circle({
+//         center: geolocation,
+//         radius: position.coords.accuracy
+//       });
+//       autocomplete.setBounds(circle.getBounds());
+//     });
+//   }
+// }
+console.log('goodbye')
