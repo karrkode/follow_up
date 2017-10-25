c9e405c6fd3ed7b445fd11c80f16f2beefbc80dd
Andrew Carr <aacarr5@gmail.com>
2017-10-25 17:18:36 -0500

    Make followers taggable

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -6,6 +6,8 @@ class Follower <ApplicationRecord
 	has_many :tags, through: :taggings
 	validates_presence_of :first_name, :last_name
 
+	attr_reader :tag_ids
+
 	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 
 
@@ -19,6 +21,15 @@ class Follower <ApplicationRecord
 
 	def self.to_ggl_api_address(addr)
 		addr.gsub(" ","+")
+	end
+
+	def add_tags(ids)
+		binding.pry
+		ids.each do |id|
+			next if id == ""
+			Tagging.create!(taggable_type:'Follower',taggable_id:self.id,tag_id:id.to_i)
+		end
+		binding.pry
 	end
 
 end
