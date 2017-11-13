6a2f3f6a3d6036657f12bc412cf2aac33db6a40f
Andrew Carr <aacarr5@gmail.com>
2017-11-13 11:45:24 -0500

    Update seed files

--- db/seeds/20171107113558_base.rb
+++ db/seeds/20171107113558_base.rb
@@ -10,11 +10,11 @@ Membership.create(user_id:me.id,organization_id:3)
 Follower.create(first_name:'Sam',last_name:'Anderson',organization_id:1,organizer_id:me.id,address:'1461 W Fullerton Ave, Chicago, IL, United States')
 
 10.times do |count|
-       user = FactoryGirl.create(:user)
-       3.times do |sub_count|
-               Membership.create!(user_id:user.id,organization_id:sub_count+1)
-               5.times {FactoryGirl.create(:follower,organizer_id:user.id,organization_id:sub_count+1)}
-       end
+	user = FactoryGirl.create(:user)
+		3.times do |sub_count|
+   		Membership.create!(user_id:user.id,organization_id:sub_count+1)
+    	5.times {FactoryGirl.create(:follower,organizer_id:user.id,organization_id:sub_count+1)}
+		end
 end
 
 Tag.create!(name:'Feminist')
--- db/seeds/uploads.rb
+++ db/seeds/uploads.rb
@@ -1,3 +1,1 @@
 5.times { FactoryGirl.create(:upload,uploader_id:1,affiliate_id:[1,2,3].sample)}
-
-puts 'seed file was loaded'
