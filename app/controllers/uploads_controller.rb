0a97111f3faa144cd997834008b32de544b333a4
Andrew Carr <aacarr5@gmail.com>
2017-11-16 12:06:24 -0600

    Fix upload controller action

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -9,12 +9,12 @@ class UploadsController < ApplicationController
     resp = S3_BUCKET.object(secure_id).upload_file(file.tempfile)
 
     if resp #refactor
-      @upload = Upload.new(name:file.original_filename,
+      @upload = Upload.create!(name:file.original_filename,
         uploader_id:current_user.id,
         s3_id:secure_id,
         affiliate_id:upload_params[:affiliate_id]
       )
-      redirect_to user_upload_path, user_id:current_user.id, id:@upload.id 
+      redirect_to user_uploads_path(user_id:current_user.id, id:@upload.id)
     else
       flash.now[:notice] = 'There was an error'
       render :new
