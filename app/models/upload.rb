560f03e6d74a3750518d9df350af797a0a6f2900
Andrew Carr <aacarr5@gmail.com>
2018-04-25 22:13:25 -0500

    Fix optional hyphen error

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -3,13 +3,11 @@ class Upload < ApplicationRecord
 	belongs_to :uploader, class_name: 'User'
 	belongs_to :organization
 
-
-	def self.generateTerf(upload,affiliate_id,file,upload_id)
+	def self.generateTerf(upload,affiliate_id,file,upload_id=nil)
 		t = Terf.create(organization_id:affiliate_id,upload_id:upload_id)
 		CSV.foreach(file.tempfile,headers:false) do |row|
-			street_num,street_name = row[2].match(/(\d+-?\d+)(.*)/)[1,2]
-			binding.pry
-			TerfEntry.create(first_name:row[0],last_name:row[1],street_number:street_num,street_name:street_name,city:row[3],state:row[4],zip:row[5],county:row[6],terf_id:t.id)
+			street_num,street_name = row[2].match(/(\d+-?\d?+)(.*)/)[1,2]
+			TerfEntry.create(first_name:row[0],last_name:row[1],street_number:street_num,street_name:street_name.strip,city:row[3],state:row[4],zip:row[5],county:row[6],terf_id:t.id)
 		end
 	end
 end
